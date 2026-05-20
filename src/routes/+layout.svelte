f1f9867b793bbac1f93d41efd9cd09a4ff6fd923
Th1nkK1D <witheep@gmail.com>
2026-05-20 11:59:54 +0700

    feat: add navigation bar page link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,15 @@
 	import '@wevisdemo/ui/styles/navbar.css';
 	import WvFooter from '@wevisdemo/ui/svelte/footer.svelte';
 	import WvNavbar from '@wevisdemo/ui/svelte/navbar.svelte';
+	import { resolve } from '$app/paths';
+	import { page } from '$app/stores';
 	import './layout.css';
 
 	let { children } = $props();
+
+	const navLinks = [{ href: '/', label: 'สำรวจเรื่องที่ สก. พูดคุย' }] as const;
+
+	let pathname = $derived($page.url.pathname);
 </script>
 
 <svelte:head>
@@ -14,7 +20,18 @@
 </svelte:head>
 
 <div class="flex h-full min-h-lvh flex-col">
-	<WvNavbar title="BMC Dashboard" homeHref="/" animated dark />
+	<WvNavbar title="BMC Dashboard" homeHref="/" animated dark>
+		<div class="flex flex-col items-center justify-center md:flex-row md:gap-5">
+			{#each navLinks as { href, label } (href)}
+				<a
+					href={resolve(href)}
+					class="wv-b7 p-3 md:p-0 {pathname === href ? 'underline' : 'hover:underline'}"
+				>
+					{label}
+				</a>
+			{/each}
+		</div>
+	</WvNavbar>
 	<main class="wv-ibmplexlooped flex-1">
 		{@render children()}
 	</main>
